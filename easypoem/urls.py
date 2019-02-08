31ce323d1662c40209dbf0698470b41fa241b654
edirab <edirab@yandex.ru>
2019-02-09 00:23:36 +0300

    bender method added

--- easypoem/urls.py
+++ easypoem/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from easypoem import views
 
 urlpatterns = [
-    path('', views.easy, name='easy'),
+    path('', views.easy, name='easypoem'),
     # path('test', views.testView, name='test'),
 ]
--- easypoem/views.py
+++ easypoem/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 def easy(request):
-    context = {}
+    breadcrumbs = [
+                    {"Главная": ''},
+                    {"Методика стихосложения": "easypoem"},
+            ]
+    context = {
+        "breadcrumbs": breadcrumbs,
+    }
     return render(request, 'easypoem/method.html', context)
